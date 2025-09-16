b2ca4010c7b11e7769247b8d4174839d5360c6da
JoaoLucasVeiga <joaolucasveigadecarvalho@gmail.com>
2025-09-16 14:02:19 -0300

    Update exercicio33.php

--- exercicio33.php
+++ exercicio33.php
@@ -5,11 +5,12 @@ Disciplina: Linguagem e Técnicas de Programacão
 Professor: José Carlos Domingues Flores
 Turma: ESOFT-2B
 Componentes:
- [RA DO JOÃO LUCAS] - João Lucas Veiga de Carvalho
- [RA DO RAFAEL] - Rafael maia
- [RA DO GABRIEL] - Gabriel Michels Cubas
+ 25291090-2 - João Lucas Veiga de Carvalho
+ 25164719-2 - Rafael maia
+ 25033056-2 - Gabriel Michels Cubas
  [RA DO JOÃO PAULO] - João Paulo Mayer
- [RA DO EMANUEL] - Emanuel Gomes de Almeida
+ 25001118-2 - Emanuel Gomes de Almeida
+ 25361613-2  - Arthur Marinho Figueira
 Data: 16/09/2025
 Descritivo: Exercício 33: Programa que determina se um número é par ou ímpar.
 *******************************************************************************/
@@ -20,3 +21,4 @@ if ($numero % 2 == 0) {
 } else {
     echo "O número $numero é ímpar.";
 }
+
