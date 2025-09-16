01f6e2cc3df8d7e9b20253cc993bde3e1f66a36b
JoaoLucasVeiga <joaolucasveigadecarvalho@gmail.com>
2025-09-16 20:36:41 -0300

    Update exercicio06.php

--- exercicio06.php
+++ exercicio06.php
@@ -5,12 +5,12 @@ Disciplina: Linguagem e Técnicas de Programacão
 Professor: José Carlos Domingues Flores
 Turma: ESOFT-2B
 Componentes:
- 25291090-2 - João Lucas Veiga de Carvalho
+25291090-2 - João Lucas Veiga de Carvalho
  25164719-2 - Rafael maia
  25033056-2 - Gabriel Michels Cubas
- [RA DO JOÃO PAULO] - João Paulo Mayer
+ 25246927-2 - João Paulo Mayer Bera
  25001118-2 - Emanuel Gomes de Almeida
- 25361613-2  - Arthur Marinho Figueira
+ 25361613-2 - Arthur Marinho Figueira
 Data: 16/09/2025
 Descritivo: Exercício 6: Faça um script que calcule a média de 3 números
 *******************************************************************************/
@@ -22,4 +22,5 @@ $media = ($num1 + $num2 + $num3) / 3;
 
 echo "Os números são: $num1, $num2 e $num3<br>";
 echo "A média é: " . number_format($media, 2, ',', '.');
+
 ?>
