c536fb31296a7bda47ac00cea0abe024ebc416e5
JoaoLucasVeiga <joaolucasveigadecarvalho@gmail.com>
2025-09-16 14:02:43 -0300

    Update exercicio43.php

--- exercicio43.php
+++ exercicio43.php
@@ -5,11 +5,12 @@ Disciplina: Linguagem e Técnicas de Programacão
 Professor: José Carlos Domingues Flores
 Turma: ESOFT-2B
 Componentes:
- [RA DO JOÃO LUCAS] - João Lucas Veiga de Carvalho
- [RA DO RAFAEL] - Rafael maia
- [RA DO GABRIEL] - Gabriel Michels Cubas
+  25291090-2 - João Lucas Veiga de Carvalho
+ 25164719-2 - Rafael maia
+ 25033056-2 - Gabriel Michels Cubas
  [RA DO JOÃO PAULO] - João Paulo Mayer
- [RA DO EMANUEL] - Emanuel Gomes de Almeida
+ 25001118-2 - Emanuel Gomes de Almeida
+ 25361613-2  - Arthur Marinho Figueira
 Data: 16/09/2025
 Descritivo: Exercício 43: Usa o operador ternário para verificar se uma pessoa pode votar (idade >= 16).
 *******************************************************************************/
@@ -18,3 +19,4 @@ $idade = 15; // Idade da pessoa
 $elegivel_para_votar = ($idade >= 16) ? "Sim, pode votar." : "Não, não pode votar.";
 echo "Idade: $idade anos. Elegível para votar? $elegivel_para_votar";
 ?>
+
