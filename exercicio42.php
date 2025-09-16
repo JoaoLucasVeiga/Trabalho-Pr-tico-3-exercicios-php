b60c6f935c85d451c75021cb83f01df29b8ce6e0
Rafa maia <122937002+Rafaelmaia1@users.noreply.github.com>
2025-09-16 20:26:59 -0300

    Update exercicio42.php

--- exercicio42.php
+++ exercicio42.php
@@ -11,11 +11,12 @@ Componentes:
             RA: 25361613-2 - Arthur Marinho Figueira
             RA: 25164719-2 - Rafael Maia
             RA: 25001118-2 - Emanuel Gomes de Almeida           
-            /*RA: 25191112-2 - João Paulo Mayer
+            RA: 25246927-2 - João Paulo Mayer
 Data: 16 de Setembro de 2025
 Descritivo: Descritivo: Expressão ternária que retorna "Par" ou "Ímpar" baseado em um número
 
 *******************************************************************************/
 $num = 11;
 $res = ($num % 2 === 0) ? "Par" : "Ímpar";
+
 echo "{$num} é {$res}" . PHP_EOL;
