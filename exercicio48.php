033ee8278f30fc1a89863d856dc8dcbe3ff23a7a
JoaoLucasVeiga <joaolucasveigadecarvalho@gmail.com>
2025-09-16 14:02:52 -0300

    Update exercicio48.php

--- exercicio48.php
+++ exercicio48.php
@@ -5,11 +5,12 @@ Disciplina: Linguagem e Técnicas de Programacão
 Professor: José Carlos Domingues Flores
 Turma: ESOFT-2B
 Componentes:
- [RA DO JOÃO LUCAS] - João Lucas Veiga de Carvalho
- [RA DO RAFAEL] - Rafael maia
- [RA DO GABRIEL] - Gabriel Michels Cubas
+ 25291090-2 - João Lucas Veiga de Carvalho
+ 25164719-2 - Rafael maia
+ 25033056-2 - Gabriel Michels Cubas
  [RA DO JOÃO PAULO] - João Paulo Mayer
- [RA DO EMANUEL] - Emanuel Gomes de Almeida
+ 25001118-2 - Emanuel Gomes de Almeida
+ 25361613-2  - Arthur Marinho Figueira
 Data: 16/09/2025
 Descritivo: Exercício 48: Usa um ternário aninhado para classificar a temperatura em Frio, Agradável ou Quente.
 *******************************************************************************/
@@ -18,3 +19,4 @@ $temperatura = 48; // Temperatura em graus Celsius
 $classificacao = ($temperatura < 20) ? "Frio" : (($temperatura <= 25) ? "Agradável" : "Quente");
 echo "Temperatura: $temperatura °C. Classificação: $classificacao";
 ?>
+
