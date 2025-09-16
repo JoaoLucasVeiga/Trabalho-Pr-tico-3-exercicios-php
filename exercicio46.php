426801eea0cdb4d4f1beeac1cf919ebd9d8a7268
JoaoLucasVeiga <joaolucasveigadecarvalho@gmail.com>
2025-09-16 20:41:02 -0300

    Update exercicio46.php

--- exercicio46.php
+++ exercicio46.php
@@ -5,12 +5,12 @@ Disciplina: Linguagem e Técnicas de Programacão
 Professor: José Carlos Domingues Flores
 Turma: ESOFT-2B
 Componentes:
- 25291090-2 - João Lucas Veiga de Carvalho
+25291090-2 - João Lucas Veiga de Carvalho
  25164719-2 - Rafael maia
  25033056-2 - Gabriel Michels Cubas
- [RA DO JOÃO PAULO] - João Paulo Mayer
+ 25246927-2 - João Paulo Mayer Bera
  25001118-2 - Emanuel Gomes de Almeida
- 25361613-2  - Arthur Marinho Figueira
+ 25361613-2 - Arthur Marinho Figueira
 Data: 16/09/2025
 Descritivo: Exercício 46 Crie uma expressão ternária que retorne "Maioridade" ou "Menoridade" baseado na idade
 *******************************************************************************/
@@ -19,4 +19,5 @@ $idade = 27;
 $resultado = ($idade >= 18) ? "maioridade" : "menoridade";
 
 echo "Idade: $idade anos - $resultado";
+
 ?>
