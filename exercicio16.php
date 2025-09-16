6a493edd6dc496b25636da9893143e627efa4d82
JoaoLucasVeiga <joaolucasveigadecarvalho@gmail.com>
2025-09-16 20:37:38 -0300

    Update exercicio16.php

--- exercicio16.php
+++ exercicio16.php
@@ -8,9 +8,9 @@ Componentes:
  25291090-2 - João Lucas Veiga de Carvalho
  25164719-2 - Rafael maia
  25033056-2 - Gabriel Michels Cubas
- [RA DO JOÃO PAULO] - João Paulo Mayer
+ 25246927-2 - João Paulo Mayer Bera
  25001118-2 - Emanuel Gomes de Almeida
- 25361613-2  - Arthur Marinho Figueira
+ 25361613-2 - Arthur Marinho Figueira
 Data: 16/09/2025
 Descritivo: Exercício 16: Verifique se 10 é maior que 5
 *******************************************************************************/
@@ -22,4 +22,5 @@ if ($numero1 > $numero2) {
 } else {
     echo "$numero1 não é maior que $numero2";
 }
+
 ?>
